790bcf558ff837c3689b9bc4915728f51682020d
Tami <tamibam@yahoo.com>
2020-10-14 10:36:25 +0300

    refactor(nvim_lsp): get rid of unnecessary definitions

--- lua/compe_nvim_lsp/source.lua
+++ lua/compe_nvim_lsp/source.lua
@@ -2,8 +2,6 @@ local Pattern = require'compe.pattern'
 
 local Source = {}
 
-Source.callback = false
-
 function Source.new(client)
   local self = setmetatable({}, { __index = Source })
   self.client = client
@@ -49,32 +47,25 @@ end
 
 function Source:complete(args)
   local params = vim.lsp.util.make_position_params()
-  local buf_has_clients = function()
-    for _ in pairs(vim.lsp.buf_get_clients()) do
-      return true
-    end
-    return false
-  end
 
-  if not buf_has_clients() then
-    Source.callback = true
-    return
+  if vim.lsp.client_is_stopped(self.client.id) then
+    return args.abort()
   end
 
   params.context = {
     triggerKind = (args.trigger_character_offset > 0 and 2 or (args.incomplete and 3 or 1))
   }
+
   if args.trigger_character_offset > 0 then
     params.context.triggerCharacter = args.context.before_char
   end
 
   self.client.request('textDocument/completion', params, function(err, _, result)
-    if err or not result then Source.callback = true return end
+    if err or not result then return args.abort() end
     args.callback({
       items = vim.lsp.util.text_document_completion_list_to_complete_items(result, '');
       incomplete = result.incomplete or false;
     })
-    Source.callback = true
   end)
 end
 
