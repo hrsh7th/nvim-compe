e7bcb06014e565b867046c6b76bc0f2c860d3bb6
hrsh7th <hrsh7th@gmail.com>
2020-11-09 21:48:47 +0900

    Improve buffer completion

--- lua/compe_buffer/buffer.lua
+++ lua/compe_buffer/buffer.lua
@@ -37,63 +37,103 @@ function Buffer.index(self)
 end
 
 function Buffer.watch(self)
-  local lnum = vim.fn.line('.')
   vim.api.nvim_buf_attach(self.bufnr, false, {
     on_lines = vim.schedule_wrap(function(_, _, _, firstline, _, new_lastline, _, _, _)
-      local new_lnum = vim.fn.line('.')
-      if lnum == new_lnum then
-        return false
-      end
-      lnum = new_lnum
-      self:add_words(table.concat(vim.api.nvim_buf_get_lines(self.bufnr, firstline, new_lastline, true), '\n'))
-      return false
+      local text
+      text = table.concat(vim.api.nvim_buf_get_lines(self.bufnr, firstline, new_lastline, true), '\n')
+      text = self:trim_ending_word(text)
+      self:add_words(text)
     end)
   })
 end
 
-function Buffer.add_words(self, text)
+--- trim_ending_word
+function Buffer.trim_ending_word(self, text)
   local buffer = text
+  local target_s = nil
+  local target_e = nil
   while true do
-    local s1, e1 = self.regex1:match_str(buffer)
-    local s2, e2 = self.regex2:match_str(buffer)
-    if s1 == nil and s2 == nil then
+    if buffer == '' then
       break
     end
 
-    if not s1 then
-      s1 = s2
-      e1 = e2
+    local s, e = self:matchstrpos(buffer)
+    if s then
+      target_s = s + #text - #buffer
+      target_e = e + #text - #buffer
+    end
+    buffer = string.sub(buffer, e and (e + 1) or 2)
+  end
+
+  -- does not match any words
+  if not target_s then
+    return ''
+  end
+
+  -- the end of buffer is not a word
+  if #text ~= target_e then
+    return text
+  end
+
+  -- remove ending word
+  return string.sub(text, 1, target_s)
+end
+
+--- add_words
+function Buffer.add_words(self, text)
+  local buffer = text
+  while true do
+    if buffer == '' then
+      break
     end
 
-    if not s2 then
-      s2 = s1
-      e2 = e1
-    end
-
-    local s = s1
-    local e = e1
-    if s1 < s2 then
-      s = s1
-      e = e2
-    elseif s2 < s1 then
-      s = s2
-      e = e2
-    elseif s1 == s2 then
-      if e1 > e2 then
-        s = s1
-        e = e1
-      elseif e2 > e1 then
-        s = s2
-        e = e2
+    local s, e = self:matchstrpos(buffer)
+    if s then
+      local word = string.sub(buffer, s + 1, e)
+      if #word > 2 and string.sub(word, #word, 1) ~= '-' then
+        self.words[word] = true
       end
     end
-
-    local word = string.sub(buffer, s + 1, e)
-    if #word > 2 and string.sub(word, #word, 1) ~= '-' then
-      self.words[word] = true
-    end
-    buffer = string.sub(buffer, e + 1)
+    buffer = string.sub(buffer, e and (e + 1) or 2)
   end
+end
+
+--- matchstrpos
+function Buffer.matchstrpos(self, text)
+  local s1, e1 = self.regex1:match_str(text)
+  local s2, e2 = self.regex2:match_str(text)
+  if s1 == nil and s2 == nil then
+    return nil, nil
+  end
+
+  if not s1 then
+    s1 = s2
+    e1 = e2
+  end
+
+  if not s2 then
+    s2 = s1
+    e2 = e1
+  end
+
+  local s = s1
+  local e = e1
+  if s1 < s2 then
+    s = s1
+    e = e2
+  elseif s2 < s1 then
+    s = s2
+    e = e2
+  elseif s1 == s2 then
+    if e1 > e2 then
+      s = s1
+      e = e1
+    elseif e2 > e1 then
+      s = s2
+      e = e2
+    end
+  end
+  return s, e
 end
 
 return Buffer
