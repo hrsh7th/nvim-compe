f798a85012aaf5d0ffdd1f2301e2db5f6f053a27
Tami <tamibam@yahoo.com>
2020-11-18 10:31:44 +0300

    Add documentation to compe/init.lua
    The way of documentation is based on LDoc
    https://github.com/lunarmodules/LDoc/blob/master/doc/doc.md
    
    Which will be used to generate documentation for nvim-compe effortlessly.
    
    This is the first file I went over, please review descriptions and
    ???/TODO and create comment.
    
    Changes:
    
    - `register_lua_source` params has changed to overcome source name conflicts
    - lower case tables.
    - adapt table-style function definition. "PS: may be weried at first but
    trust me it makes since under lua object/table style "

--- lua/compe/init.lua
+++ lua/compe/init.lua
@@ -1,84 +1,128 @@
-local Completion = require'compe.completion'
-local Debug = require'compe.debug'
-local Source = require'compe.completion.source'
-local VimBridge = require'compe.completion.source.vim_bridge'
+--- compe public inteface module
+--
+-- This module wraps a number of functions from other modules into a a
+-- convenient  public interface that can be used to access all nvim-compe main
+-- functionaries
+-- TODO: is it?.
+--
+-- Dependencies: `compe.completion`, `compe.debug`, `compe.completion.source`,
+-- `compe.completion.source.vim_bridge`
+-- @module compe
 
-local Compe = {}
+local completion = require'compe.completion'
+local debug = require'compe.debug'
+local source = require'compe.completion.source'
+local vim_bridge = require'compe.completion.source.vim_bridge'
 
---- new
-function Compe.new()
-  local self = setmetatable({}, { __index = Compe })
-  self.completion = Completion.new()
+local compe = {}
+
+--- sets compe-nvim public interface object.
+-- @return - A new compe object.
+compe.new = function()
+  local self = setmetatable({}, { __index = compe })
+  self.completion = completion.new()
   return self
 end
 
---- register_lua_source
-function Compe.register_lua_source(self, id, source)
-  self.completion:register_source(Source.new(id, source))
+--- registers a new lua source
+-- passes params using new func from `source` to `completion` register_source
+-- func.
+-- @param id string representing the source name.
+-- @param obj table representing nvim-compe source.
+-- @function register_lua_source
+compe.register_lua_source = function(self, id, obj)
+  self.completion:register_source(source.new(id, obj))
 end
 
---- register_vim_source
-function Compe.register_vim_source(self, id)
-  self.completion:register_source(Source.new(id, VimBridge.new(id)))
+--- registers a new vim source
+-- passes params using new func from `source` and `vim_bridge` to `completion`
+-- register_source func.
+-- @param id string representing the source name.
+-- @function register_vim_source
+compe.register_vim_source = function(self, id)
+  self.completion:register_source(source.new(id, vim_bridge.new(id)))
 end
 
---- unregister_source
-function Compe.unregister_source(self, id)
+--- unregisters a vim or lua source
+-- passes params using to `completion` unregister_source func.
+-- @param id string representing the source name.
+-- @function unregister_source
+compe.unregister_source = function(self, id)
   self.completion:unregister_source(id)
 end
 
---- on_complete_changed
-function Compe.on_complete_changed(self)
+--- wraps `completion:on_complete_changed`
+-- ???
+-- @param self
+-- @function on_complete_changed
+compe.on_complete_changed = function(self)
   local status, value = pcall(function() self.completion:on_complete_changed() end)
   if not(status) then
-    Debug:log(value)
+    debug:log(value)
   end
 end
 
---- on_complete_done
-function Compe.on_complete_done(self)
+--- wraps `completion:on_complete_done`
+-- ???
+-- @param self
+-- @function on_complete_done
+compe.on_complete_done = function(self)
   local status, value = pcall(function() self.completion:on_complete_done() end)
   if not(status) then
-    Debug:log(value)
+    debug:log(value)
   end
 end
 
---- on_text_changed
-function Compe.on_text_changed(self)
+--- wraps `completion:on_text_changed`
+-- Event that triggers on text change ???
+-- @param self
+-- @function on_text_changed
+compe.on_text_changed = function(self)
   local status, value = pcall(function() self.completion:on_text_changed() end)
   if not(status) then
-    Debug:log(value)
+    debug:log(value)
   end
 end
 
---- on_insert_leave
-function Compe.on_insert_leave(self)
+--- wraps `completion:on_text_changed`
+-- Event that triggers on insert leave ???
+-- @param self
+-- @function on_insert_leave
+compe.on_insert_leave = function(self)
   local status, value = pcall(function() self.completion:on_insert_leave() end)
   if not(status) then
-    Debug:log(value)
+    debug:log(value)
   end
 end
 
---- on_manual_complete
-function Compe.on_manual_complete(self)
+--- wraps `completion:on_manual_complete`
+-- ???
+-- @param self
+-- @function on_manual_complete
+compe.on_manual_complete = function(self)
   local status, value = pcall(function() self.completion:on_manual_complete() end)
   if not(status) then
-    Debug:log(value)
+    debug:log(value)
   end
 end
 
--- add_history
-function Compe.add_history(self, word)
+--- wraps `completion:add_history`
+-- ???
+-- @param self
+-- @function add_history
+compe.add_history = function(self, word)
   self.completion:add_history(word)
 end
 
---- clear
-function Compe.clear(self)
+--- wraps `completion:clear`
+-- ???
+-- @param self
+-- @function clear
+compe.clear = function(self)
   local status, value = pcall(function() self.completion:clear() end)
   if not(status) then
-    Debug:log(value)
+    debug:log(value)
   end
 end
 
-return Compe.new()
-
+return compe.new()
