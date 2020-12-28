94b2d713a3ab59698ce272a9e3f9466f9ac96adf
hrsh7th <hrsh7th@gmail.com>
2020-12-29 03:12:18 +0900

    Fix #60

--- lua/compe/helper.lua
+++ lua/compe/helper.lua
@@ -5,14 +5,14 @@ local Helper = {}
 --- datermine
 Helper.datermine = function(context, option)
   local trigger_character_offset = 0
-  if option and option and option.trigger_characters and context.before_char ~= ' ' then
+  if option and option.trigger_characters and context.before_char ~= ' ' then
     if vim.tbl_contains(option.trigger_characters, context.before_char) then
       trigger_character_offset = context.col
     end
   end
 
   local keyword_pattern_offset
-  if option.keyword_pattern then
+  if option and option.keyword_pattern then
     keyword_pattern_offset = Pattern.get_pattern_offset(context.before_line, option.keyword_pattern)
   end
 
