01368dd9eba55f06cce12c5498197b49803cfaf2
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-01-11 12:47:57 +0530

    Ultisnips completion source improvements:
    Check ultisnips is installed before registering
    
    Remove the existence check for Ultisnips function at each completion
    step.
    
    Use user_data instead of userdata as vim internally make use of it.
    
    Use snake case in codebase.

--- after/plugin/compe_ultisnips.vim
+++ after/plugin/compe_ultisnips.vim
@@ -1,4 +1,4 @@
-if exists('g:loaded_compe_ultisnips')
+if match(&runtimepath, 'UltiSnips') == -1 || exists('g:loaded_compe_ultisnips')
   finish
 endif
 let g:loaded_compe_ultisnips = v:true
--- lua/compe_ultisnips/init.lua
+++ lua/compe_ultisnips/init.lua
@@ -15,39 +15,35 @@ function M:datermine(context)
 end
 
 function M:complete(args)
-  if vim.fn.exists("*UltiSnips#SnippetsInCurrentScope") == 0 then
+  local received_snippets = vim.call('UltiSnips#SnippetsInCurrentScope')
+  if vim.tbl_isempty(received_snippets) then
     args.abort()
     return
   end
-  local receivedSnippets = vim.call('UltiSnips#SnippetsInCurrentScope')
-  if vim.tbl_isempty(receivedSnippets) then
-    args.abort()
-    return
-  end
-  local completionList = {}
-  for key, value in pairs(receivedSnippets) do
+  local completion_list = {}
+  for key, value in pairs(received_snippets) do
     local item = {
       word =  key,
       abbr =  key,
-      userdata = value,
+      user_data = value,
       kind = 'Snippet',
       dup = 1
     }
-    table.insert(completionList, item)
+    table.insert(completion_list, item)
   end
   args.callback{
-    items = completionList
+    items = completion_list
   }
 end
 
 function M:documentation(args)
-  local completedItem = args.completed_item
-  local userdata = completedItem.userdata
-  if userdata == nil or userdata == '' then
+  local completed_item = args.completed_item
+  local user_data = completed_item.user_data
+  if user_data == nil or user_data == '' then
     args.abort()
     return
   end
-  args.callback(userdata)
+  args.callback(user_data)
 end
 
 return M
