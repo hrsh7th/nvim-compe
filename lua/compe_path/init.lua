8d6d0c32c4e611255290e95185590f3b27234bbe
hrsh7th <hrsh7th@gmail.com>
2021-02-02 02:29:51 +0900

    WIP

---
+++ lua/compe_path/init.lua
@@ -0,0 +1,137 @@
+local compe = require'compe'
+
+local BASENAME_PATTERN = [[\%([^/\\:\*?<>\|]\)]]
+local BASENAME_REGEX = vim.regex(BASENAME_PATTERN .. '*$')
+local DIRNAME_REGEX = vim.regex(([[\%(/PAT*\)*\ze/PAT*$]]):gsub('PAT', BASENAME_PATTERN))
+
+local Source = {}
+
+--- get_metadata
+Source.get_metadata = function(_)
+  return {
+    sort = false,
+    priority = 100,
+  }
+end
+
+--- determine
+Source.determine = function(_, context)
+  return compe.helper.determine(context, {
+    keyword_pattern = ([[/\zs%s*$]]):format(BASENAME_PATTERN),
+    trigger_characters = { '/' }
+  })
+end
+
+--- complete
+Source.complete = function(self, args)
+  local basename = self:_basename(args.context)
+  local dirname = self:_dirname(args.context)
+  if not basename or not dirname then
+    return args.abort()
+  end
+
+  local stat = self:_stat(dirname)
+  if not stat then
+    return args.abort()
+  end
+
+  self:_candidates(dirname, vim.schedule_wrap(function(err, candidates)
+    if err then
+      return args.abort()
+    end
+    args.callback({
+      items = candidates,
+    })
+  end))
+end
+
+--- _basename
+Source._basename = function(_, context)
+  local s, e = BASENAME_REGEX:match_str(context.before_line)
+  if not s then
+    return nil
+  end
+  return string.sub(context.before_line, s + 1, e)
+end
+
+--- _dirname
+Source._dirname = function(self, context)
+  local s, e = DIRNAME_REGEX:match_str(context.before_line)
+  if not s then
+    return nil
+  end
+  local dirname = string.sub(context.before_line, s + 1, e)
+  local prefix = string.sub(context.before_line, 1, s + 1)
+
+  local buf_dirname = vim.fn.expand(('#%d:p:h'):format(context.bufnr))
+  if prefix:match('%../$') then
+    return vim.fn.resolve(buf_dirname .. '/../' .. dirname)
+  elseif prefix:match('%./$') then
+    return vim.fn.resolve(buf_dirname .. '/' .. dirname)
+  elseif prefix:match('~/$') then
+    return vim.fn.resolve('~/' .. dirname)
+  elseif prefix:match('/$') then
+    local accept = true
+    -- Ignore HTML closing tags
+    accept = accept and not prefix:match('</$')
+    -- Ignore math calculation
+    accept = accept and not prefix:match('%d%s*/$')
+    -- Ignore / comment
+    accept = accept and (not prefix:match('^%s*/$') or not self:_is_slach_comment())
+    -- Ignore URL scheme
+    accept = accept and not prefix:match('%a+:/$') and not prefix:match('%a+://$')
+    if accept then
+      return vim.fn.resolve('/' .. dirname)
+    end
+  end
+  return nil
+end
+
+Source._stat = function(_, path)
+  local stat = vim.loop.fs_stat(path)
+  if stat then
+    return stat
+  end
+  return nil
+end
+
+Source._candidates = function(_, dirname, callback)
+  vim.loop.fs_scandir(dirname, function(err, fs)
+    if err then
+      return callback(err, nil)
+    end
+
+    local items = {}
+    while true do
+      local name, type, e = vim.loop.fs_scandir_next(fs)
+      if e then
+        return callback(type, nil)
+      end
+      if not name then
+        break
+      end
+      if type == 'directory' then
+        table.insert(items, {
+          word = name,
+          abbr = '/' .. name,
+          menu = '[Dir]'
+        })
+      else
+        table.insert(items, {
+          word = name,
+          abbr = name,
+          menu = '[File]'
+        })
+      end
+    end
+    print(items)
+    callback(nil, items)
+  end)
+end
+
+Source._is_slach_comment = function(_)
+  return not not (vim.o.commentstring or ''):match('//|/%*')
+end
+
+return Source
+
