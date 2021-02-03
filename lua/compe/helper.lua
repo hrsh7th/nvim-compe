0766e01f6c515a591c14388ecfeb14b6b99eb6f6
hrsh7th <hrsh7th@gmail.com>
2021-02-03 23:47:57 +0900

    Add LSC source (#128)

--- lua/compe/helper.lua
+++ lua/compe/helper.lua
@@ -32,5 +32,57 @@ Helper.get_default_pattern = function()
   return Pattern.get_default_pattern()
 end
 
+--- convert_lsp
+Helper.convert_lsp = function(request_position, response)
+  local completion_items = vim.tbl_islist(response or {}) and response or response.items or {}
+
+  local complete_items = {}
+  for _, completion_item in pairs(completion_items) do
+    local label = completion_item.label
+    local insert_text = completion_item.insertText or label
+
+    local word = ''
+    local abbr = ''
+    if completion_item.insertTextFormat == 2 then
+      word = label
+      abbr = label
+
+      local text = word
+      if completion_item.textEdit ~= nil then
+        text = completion_item.textEdit.newText or text
+      elseif completion_item.insertText ~= nil then
+        text = completion_item.insertText or text
+      end
+      if word ~= text then
+        abbr = abbr .. '~'
+      end
+      word = string.match(text, '[^%s=%(%$"\']+')
+    else
+      word = insert_text
+      abbr = label
+    end
+    word = string.gsub(word, '^%s*|%s*$', '')
+
+    table.insert(complete_items, {
+      word = word;
+      abbr = abbr;
+      preselect = completion_item.preselect or false;
+      kind = vim.lsp.protocol.CompletionItemKind[completion_item.kind] or nil;
+      user_data = {
+        compe = {
+          request_position = request_position;
+          completion_item = completion_item;
+        };
+      };
+      filter_text = completion_item.filterText or nil;
+      sort_text = completion_item.sortText or nil;
+    })
+  end
+  return {
+    items = complete_items,
+    incomplete = response.incomplete or false,
+  }
+end
+
 return Helper
 
