1955737c4d6c0f279d196224c0d472bfcdd5655b
elianiva <dicha.arkana03@gmail.com>
2021-02-04 22:08:43 +0700

    refactor: cleanup, added checks

--- lua/compe_treesitter/init.lua
+++ lua/compe_treesitter/init.lua
@@ -1,3 +1,4 @@
+local ts_exists = pcall(require, "nvim-treesitter")
 local ts_locals = require('nvim-treesitter.locals')
 local parsers = require('nvim-treesitter.parsers')
 local ts_utils = require('nvim-treesitter.ts_utils')
@@ -18,6 +19,13 @@ function Source.get_metadata(_)
 end
 
 function Source.determine(_, context)
+  if not ts_exists then
+    error("You need to install nvim-treesitter!")
+    return {}
+  end
+
+  if not parsers.has_parser() then return {} end
+
   return compe.helper.determine(context)
 end
 
@@ -46,39 +54,31 @@ function Source._prepare_match(self, match, kind)
   return matches
 end
 
-function Source._get_treesitter_items(self)
-  if parsers.has_parser() then
-    local complete_items = {}
+function Source.complete(self, context)
+  local complete_items = {}
 
-    local at_point = ts_utils.get_node_at_cursor()
-    local line_current = vim.api.nvim_win_get_cursor(0)[1]
+  local at_point = ts_utils.get_node_at_cursor()
+  local line_current = vim.api.nvim_win_get_cursor(0)[1]
 
-    for name, definitions in ipairs(ts_locals.get_definitions(0)) do
-      local matches = self:_prepare_match(definitions, name)
+  for name, definitions in ipairs(ts_locals.get_definitions(0)) do
+    local matches = self:_prepare_match(definitions, name)
 
-      for _, match in ipairs(matches) do
-        local node = match.node
-        local node_scope = self:_get_smallest_context(node)
-        local start_line_node, _, _= node:start()
-        local node_text = ts_utils.get_node_text(node, 0)[1]
+    for _, match in ipairs(matches) do
+      local node = match.node
+      local node_scope = self:_get_smallest_context(node)
+      local start_line_node, _, _= node:start()
+      local node_text = ts_utils.get_node_text(node, 0)[1]
 
-        if node_text
-          and (not node_scope or ts_utils.is_parent(node_scope, at_point))
-          and (start_line_node <= line_current) then
-          table.insert(complete_items, {word = node_text, kind = match.kind})
-        end
+      if node_text
+        and (not node_scope or ts_utils.is_parent(node_scope, at_point))
+        and (start_line_node <= line_current) then
+        table.insert(complete_items, {word = node_text, kind = match.kind})
       end
     end
-
-    return complete_items
   end
 
-  return {}
-end
-
-function Source.complete(self, context)
   context.callback({
-    items = self:_get_treesitter_items(),
+    items = complete_items,
     incomplete = true
   })
 end
