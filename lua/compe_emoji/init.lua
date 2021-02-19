cbe5a86533bbd4d0af60d060bce7ca03ca8de3d7
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-02-19 18:16:58 +0100

    Implement emoji autocompletion

---
+++ lua/compe_emoji/init.lua
@@ -0,0 +1,29 @@
+local compe = require'compe'
+
+local Source = {}
+
+Source.get_metadata = function(_)
+  return {
+    priority = 80,
+    dup = 1,
+    menu = '[Emoji]'
+  }
+end
+
+Source.determine = function(_, context)
+  return compe.helper.determine(context, {
+    keyword_pattern = [[:\w*$]]
+  })
+end
+
+-- Load emoji data from auto-generated file:
+Source._items = require('compe_emoji.data')
+
+Source.complete = function(self, args)
+  args.callback({
+    items = self._items,
+    incomplete = true,
+  })
+end
+
+return Source
