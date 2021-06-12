8d8737bef96d68e239000dad04fb705f4e536827
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-06-12 01:09:17 -0400

    support vim and shellcode interpolation (no python)

--- lua/compe_ultisnips/init.lua
+++ lua/compe_ultisnips/init.lua
@@ -25,7 +25,16 @@ local function get_snippet_preview(data, args)
         local s, e = EXPRESSION_REGEX:match_str(line)
         while s do
           local expr = string.sub(line, s+2, e-1)
-          local evaluated = vim.api.nvim_eval(expr)
+          local prefix = string.sub(expr, 1, 2)
+          local evaluated
+          if prefix == '!v' then
+            evaluated = vim.api.nvim_eval(string.sub(expr, 3))
+          elseif prefix == '!p' then
+            -- TODO: implement python interpolation (value of snip.rv)
+            evaluated = expr
+          else
+            evaluated = vim.fn.system(expr)
+          end
           line = string.sub(line, 1, s+1) .. evaluated .. string.sub(line, e+1)
           s, e = EXPRESSION_REGEX:match_str(line)
         end
