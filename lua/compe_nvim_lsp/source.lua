83b33e70f4b210ebfae86a2ec2d054ca31f467dd
hrsh7th <hrsh7th@gmail.com>
2021-07-08 00:30:46 +0900

    Fix #451

--- lua/compe_nvim_lsp/source.lua
+++ lua/compe_nvim_lsp/source.lua
@@ -99,17 +99,29 @@ function Source.documentation(self, args)
 end
 
 --- _create_document
-function Source._create_document(self, filetype, completion_item)
-  local doc = completion_item.documentation or {}
-  if type(doc) == "string" then
-    doc = string.format("```%s\n%s\n```", filetype, doc)
+function Source._create_document(_, filetype, completion_item)
+  local detail = (function()
+    if completion_item.detail then
+      return string.format("```%s\n%s\n```", filetype, completion_item.detail)
+    end
+  end)()
+  local doc = (function()
+    local doc = completion_item.documentation or {}
+    if type(doc) == "string" then
+      doc = string.format("```%s\n%s\n```", filetype, doc)
+    else
+      doc = vim.tbl_deep_extend('force', {}, doc)
+    end
+    return doc
+  end)()
+  local items = {}
+  if detail then
+    table.insert(items, detail)
   end
-  -- HACK: make a deepcopy to prevent double escaping of plaintext
-  -- see also https://github.com/hrsh7th/nvim-compe/issues/440
-  if type(doc) == "table" then
-    doc = vim.tbl_deep_extend("force", {}, doc)
+  if doc then
+    table.insert(items, doc)
   end
-  return doc and util.convert_input_to_markdown_lines(doc) or {}
+  return util.convert_input_to_markdown_lines(items) or {}
 end
 
 --- _get_paths
